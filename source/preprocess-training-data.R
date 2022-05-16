44a27e516d937b4171018192331b35f00900c788
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-05-16 13:37:55 -0400

    Update preprocess-training-data.R

--- source/preprocess-training-data.R
+++ source/preprocess-training-data.R
@@ -20,6 +20,8 @@ preprocess.training.data <- function(
     colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^point_y$", replacement =   "latitude");
     colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^lon$",     replacement =  "longitude");
     colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^lat$",     replacement =   "latitude");
+    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^x$", replacement =  "longitude")
+    colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^y$", replacement =   "latitude");
     colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^vv$",      replacement =         "VV");
     colnames(DF.output) <- gsub(x = colnames(DF.output), pattern = "^vh$",      replacement =         "VH");
 
