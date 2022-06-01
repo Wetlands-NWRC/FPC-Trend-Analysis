6344b13bc28e81cd4b9a39aecf95b2a5550cc6e5
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-06-01 13:46:42 -0400

    updated the selector

--- source/query.R
+++ source/query.R
@@ -2,5 +2,5 @@
 select.landcover <- function(DF.input = NULL, land.covers = NULL) {
    if(is.null(land.covers)) {return(DF.input)}
 
-   return(DF.input[DF.input$land_cover == land.covers, ])
+   return(DF.input[DF.input$land_cover %in% land.covers, ])
 }
