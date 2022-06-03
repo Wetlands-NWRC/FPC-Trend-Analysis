bd9867788f12c1aa8a92e7d0e181746b180aa6d1
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-06-03 14:10:11 -0400

    stubbed out query by training data by year func

--- source/query.R
+++ source/query.R
@@ -1,6 +1,12 @@
 
-select.landcover <- function(DF.input = NULL, land.covers = NULL) {
+query.landcover <- function(DF.input = NULL, land.covers = NULL) {
    if(is.null(land.covers)) {return(DF.input)}
 
    return(DF.input[DF.input$land_cover %in% land.covers, ])
+}
+
+query.year <- function(DF.input = NULL, year = NULL){
+   if (is.null(year)) {return(DF.input)}
+
+
 }
