6f234fba03d437917854a3ed496c3ef3ab4b85bb
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-07-07 15:19:23 -0400

    updates

--- fpcrunner/dirutils.py
+++ fpcrunner/dirutils.py
@@ -1,11 +1,35 @@
 import os
 import shutil
 
-from typing import List
+from typing import List, Union
+
+
+class MountPoint:
+    def __init__(self, mount: str) -> None:
+        """Create a Mount Point object. This object represents a mount point, 
+        which could be a path to a physical drive location
+
+        Args:
+            mount (str): a str representing a real drive location
+        """
+        self._mnt = mount
+    
+    def __bool__(self) -> bool:
+        return os.path.exists(self._mnt)
+
+    def __str__(self) -> str:
+        return f"Mount Point: {self.dest}"
+    
+    def make_mount(self) -> None:
+        if not self:
+            os.makedirs(self._mnt)
+            return None
+        else:
+            return None
 
 
 class MoveToMountPoint:
-    def __init__(self, src: str, dest: str) -> None:
+    def __init__(self, src: str, dest: Union[str, MountPoint]) -> None:
         self.src = src
         self.dest = dest
     
@@ -15,5 +39,4 @@ class MoveToMountPoint:
     def mk_mnt_dir(self) -> None:
         os.makedirs(self.dest, exist_ok=True)
     
-    def ignore(self) -> List[str]:
-        pass
+
