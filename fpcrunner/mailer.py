3c2c3c3035cf7781bfd551d7bd904b0f49f7f922
Ryan Hamilton <ryangilberthamilton@gmail.com>
2022-07-07 12:47:18 -0400

    added mailer class

--- fpcrunner/mailer.py
+++ fpcrunner/mailer.py
@@ -1,62 +1,64 @@
-import json
-import yagmail
+
+import smtplib
+import ssl
+import os
+
+from dataclasses import dataclass
+from email.message import EmailMessage
+from typing import Dict
+
 import runner
 
-class MailerConfig:
-
-    def __init__(self, config) -> None:
-        self.config = config
-
-        with open(self.config, 'r') as config:
-            data = json.load(config)
-
-            self.username = data.get("username")
-            self.password = data.get('password')
-            self.recipient = data.get('recipient')
-
-
+    
 class Mailer:
     # TODO look into creating my own SMTP connection for sending emails
     """Takes an exit code and sends the appropate notification 
     to the spcified recipent
     """
-    def __init__(self, exit_code: runner.ExitCode, username, password, recipt, send_notification: bool = True) -> None:
-        self.yag = yagmail.SMTP(username, password)
-        self.recp = recipt
-        
-        if exit_code == 0:
-            message = self.on_sucesses()
-        else:
-            message = self.on_fail()
-        
-        if send_notification:
-            self.send_mail(message)
-
-    def on_fail(self) -> list:
-        message_conf = {
-            "contents": ["FPC pipeline failed!"],
-            "subject": "Pipeline Failed" 
-        }
-       
-        return message_conf
+    def __init__(self, exit_code: runner.ExitCode) -> None:
+        self._exit_code = exit_code
+        self.__email_sender = 'fnwrc.pc.mailer@gmail.com'
+        self.__password = os.environ.get("mailer_password", None)
+        self.__email_receiver = 'ryangilberthamilton@gmail.com'
+        self._em = EmailMessage()
     
-    def on_sucesses(self) -> list:
-        message_conf = {
-            "contents": ["FPC pipeline completed successfully!"],
-            "subject": "Pipeline Completed" 
-        }
-       
-        return message_conf
-    
-    def send_mail(self, message_conf: Dict[str, Union[List[str], str]]) -> None:
-        contents = message_conf.get("contents")
-        subject = message_conf.get("subject")
+        self._em['From'] = self.__email_sender
         
-        self.yag.send(
-            to=self.recp,
-            subject=subject,
-            contents=contents
-        )
-
+    
+    def _on_success(self) -> None:
+        """ Set success body and subject line """
+        self._em['Subject'] = 'Alert: Pipeline Successful'
+        
+        body = """ FPCA Pipeline has completed and has been sucssfully moved
+        to the defined mount point.
+        mnt <insert mount point>
+        """
+        self._em.set_content(body)
         return None
+    
+    
+    def _on_failure(self) -> EmailMessage:
+        """ sets props of unscessfull run """
+        self._em['Subject'] = 'Alert: Pipeline has failed'
+        body = """FPCA Pipeline has Exited with a Non Zero Status. Please check
+        logs for more information"""
+        self._em.set_content(body)
+        return None
+         
+
+    def send_mail(self) -> None:
+        if self._exit_code > 0:
+            self._on_failure()
+        else:
+            self._on_success()
+
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(self.__email_sender, self.__password)
+            smtp.sendmail(self.__email_sender, self.__email_receiver,
+                          self._em.as_string())
+    
+        return None
+        
   
