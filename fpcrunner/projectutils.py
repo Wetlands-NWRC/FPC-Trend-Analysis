3b74cd417d98a73e95ba07ab8eae053eca6e22d3
Ryan <ryangilberthamilton@gmail.com>
2022-10-18 15:14:17 -0400

    added case creation functions

--- fpcrunner/projectutils.py
+++ fpcrunner/projectutils.py
@@ -1,7 +1,9 @@
 import os
 import shutil
+from itertools import combinations
+from typing import Iterable, List
 
-
+import yaml
 
 
 def format_image_dir(path: str, destination: str):
@@ -31,3 +33,74 @@ def format_image_dir(path: str, destination: str):
         dst_path = os.path.join(destination, root, dst)
         shutil.copytree(src_path, dst_path, dirs_exist_ok=True)
 
+
+def case_combinations(working_dir: str, land_cover: List[str], start_idx=1, target_variables: List[str] = None, create_config: bool = True):
+    if start_idx < 1:
+        raise Exception(f'start_idx of 0 is reserved non case specific data')
+    
+    target_variables = ['VV', 'VH'] if target_variables is None else target_variables
+
+    comb = combinations(land_cover, 2)
+
+    for idx, comb in enumerate(combinations, start=1):
+        suffix = "_".join(comb)
+        prefix = f'{idx:0>3}_'
+        concat = prefix + suffix
+        
+        for tgt in target_variables:
+            path = os.path.join(concat, tgt)
+            if not os.path.exists(path):
+                os.makedirs(path)
+        
+            if not create_config:
+                return None
+
+            cfg = os.path.join(path, 'conf.yml')
+
+            document = {
+                'dataDir': '/home/wetlands/projects/FPCA/05_Remove_Mean/williston-a/000-data',
+                'trainingDataDir': 'training_data',
+                'targetVariable': tgt,
+                'landCover': [*comb],
+                'remove_mean': True,
+            }
+
+            with open(cfg, 'w') as stream:
+                yaml.dump(document, stream)
+
+    return None
+
+
+def versus(working_dir: str, trg: str, versus: Iterable[str], start_idx: int = 1, target_variables: List[str] = None,
+           create_config: bool = True):
+    ###
+    if start_idx < 1:
+        raise Exception(f'start_idx of 0 is reserved non case specific data')
+    
+    target_variables = ['VV', 'VH'] if target_variables is None else target_variables
+
+    for idx, land_cover in enumerate(versus, start=start_idx):
+        suffix = f'{trg}_{land_cover}'
+        prefix = f'{idx:0>3}_'
+        concat = prefix + suffix
+
+        for tgt in target_variables:
+            path = os.path.join(concat, tgt)
+            if not os.path.exists(path):
+                os.makedirs(path)
+        
+            if not create_config:
+                return None
+
+            cfg = os.path.join(path, 'conf.yml')
+
+            document = {
+                'dataDir': '/home/wetlands/projects/FPCA/05_Remove_Mean/williston-a/000-data',
+                'trainingDataDir': 'training_data',
+                'targetVariable': tgt,
+                'landCover': [trg, land_cover],
+                'remove_mean': True,
+            }
+
+            with open(cfg, 'w') as stream:
+                yaml.dump(document, stream)
