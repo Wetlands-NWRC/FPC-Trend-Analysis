df7cb933baccc914f1aa3b739ac459f939a40e16
Ryan <ryangilberthamilton@gmail.com>
2022-10-18 13:59:24 -0400

    added fpc standard workflow

--- fpcrunner/workflow.py
+++ fpcrunner/workflow.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from typing import Dict
 from fpcrunner import rasutil as ru
 from fpcrunner import dirutils
 from fpcrunner import mailer
 from fpcrunner import runner
+from fpcrunner import toolbox as tbx
 
 
 class NonZeroExitStatus(Exception):
@@ -11,11 +13,44 @@ class NonZeroExitStatus(Exception):
 
 
 class Workflow(ABC):
-    _parameters: Dict[int, str]
-    
+
     @abstractmethod
-    def run(self):
+    def __post_init__(self):
         pass
+
+
+@dataclass
+class FpcStandard(Workflow):
+    config: str
+    output_dir: str
+    tiff_name: str
+    case_name: str = None
+
+    def __post_init__(self):
+        print("Starting FPC Standard Workflow...")
+
+        fac = runner.Facilitator(
+            config_file=self.config,
+            out_dir=self.output_dir 
+        )
+        fac.run()
+        exit_code = fac.exit_code
+        
+        email = mailer.Mailer(
+            exit_code=exit_code,
+            case_name='FPC-Case'
+        )
+
+        email.send_mail()
+
+        if exit_code > 0:
+            raise Exception(f"Pipeline has failed {str(exit_code)}")
+        
+        print("Generating Scores GeoTIFF...")
+        tbx.MosaicScores(
+            working_dir=self.output_dir,
+            filename=self.tiff_name
+        )
 
 
 class FPCExperiment(Workflow):
